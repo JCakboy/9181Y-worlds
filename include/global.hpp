47acb195c11bd0831e8ae1d8584b59174a65c0c9
JCakboy <36982207+JCakboy@users.noreply.github.com>
2019-04-02 16:46:52 -0700

    Added ports and definitions

---
+++ include/global.hpp
@@ -0,0 +1,48 @@
+#ifndef _PORTS_HPP_
+#define _PORTS_HPP_
+
+#include "main.h"
+
+namespace ports {
+
+  // Motors
+  extern pros::Motor * port1;
+  extern pros::Motor * port2;
+  extern pros::Motor * port3;
+  extern pros::Motor * port4;
+  extern pros::Motor * port5;
+  extern pros::Motor * port6;
+  extern pros::Motor * port7;
+  extern pros::Motor * port8;
+  extern pros::Motor * port9;
+  extern pros::Motor * port10;
+  extern pros::Motor * port11;
+  extern pros::Motor * port12;
+  extern pros::Motor * port13;
+  extern pros::Motor * port14;
+  extern pros::Motor * port15;
+  extern pros::Motor * port16;
+  extern pros::Motor * port17;
+  extern pros::Motor * port18;
+  extern pros::Motor * port19;
+  extern pros::Motor * port20;
+  extern pros::Motor * port21;
+
+  // Port mapping
+  extern pros::Motor * leftFrontDriveMotor;
+  extern pros::Motor * leftBackDriveMotor;
+  extern pros::Motor * rightFrontDriveMotor;
+  extern pros::Motor * rightBackDriveMotor;
+  extern pros::Motor * liftMotor;
+  extern pros::Motor * intakeMotor;
+  extern pros::Motor * indexMotor;
+  extern pros::Motor * flywheelMotor;
+
+  // Vision
+  extern pros::Vision * flagVision;
+
+  // ADI (3-wire) ports
+
+}
+
+#endif
---
+++ src/global.cpp
@@ -0,0 +1,44 @@
+#include "main.h"
+
+// Ports initialization
+namespace ports {
+
+  // Motors
+  pros::Motor * port1 = NULL;
+  pros::Motor * port2 = NULL;
+  pros::Motor * port3 = NULL;
+  pros::Motor * port4 = NULL;
+  pros::Motor * port5 = NULL;
+  pros::Motor * port6 = NULL;
+  pros::Motor * port7 = NULL;
+  pros::Motor * port8 = NULL;
+  pros::Motor * port9 = NULL;
+  pros::Motor * port10 = NULL;
+  pros::Motor * port11 = NULL;
+  pros::Motor * port12 = NULL;
+  pros::Motor * port13 = NULL;
+  pros::Motor * port14 = NULL;
+  pros::Motor * port15 = NULL;
+  pros::Motor * port16 = NULL;
+  pros::Motor * port17 = NULL;
+  pros::Motor * port18 = NULL;
+  pros::Motor * port19 = NULL;
+  pros::Motor * port20 = NULL;
+  pros::Motor * port21 = NULL;
+
+  // Port mapping
+  pros::Motor * leftFrontDriveMotor = NULL;
+  pros::Motor * leftBackDriveMotor = NULL;
+  pros::Motor * rightFrontDriveMotor = NULL;
+  pros::Motor * rightBackDriveMotor = NULL;
+  pros::Motor * liftMotor = NULL;
+  pros::Motor * intakeMotor = NULL;
+  pros::Motor * indexMotor = NULL;
+  pros::Motor * flywheelMotor = NULL;
+
+  // Vision
+  pros::Vision * flagVision = NULL;
+
+  // ADI (3-wire) ports
+
+}
