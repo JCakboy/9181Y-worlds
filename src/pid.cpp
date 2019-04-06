dc22abb6ee0e9fd8d35a9492d78560b4ed17adef
Jimmy Nimavat <jaimin2468@gmail.com>
2019-04-06 09:32:02 -0700

    added: comments to pid.cpp for clarity

--- src/pid.cpp
+++ src/pid.cpp
@@ -17,6 +17,7 @@ void PID::resetEncoders() {
 }
 
 int PID::checkPower(int power) {
+  //check if power is within the positive value ranges
   if(power > 0) {
     if(power > MAX_POWER) {
       power = MAX_POWER;
@@ -25,6 +26,7 @@ int PID::checkPower(int power) {
       power = MIN_POWER;
     }
   }
+  //check if power is within the negative value ranges
   else if(power < 0) {
     if(power < -MAX_POWER) {
       power = -MAX_POWER;
@@ -36,6 +38,7 @@ int PID::checkPower(int power) {
   return power;
 }
 
+//makes sure robot drives straight
 void PID::driveStraight(int power) {
   int kp = 0;
   int master = 0;
@@ -43,6 +46,7 @@ void PID::driveStraight(int power) {
   int powerLeft = 0;
   int powerRight = 0;
 
+  //make master the side that is moving more than the other side
   if(abs(frontLeftDrive->get_position())  > abs(frontRightDrive->get_position())) {
     master = frontLeftDrive->get_position();
     partner = frontRightDrive->get_position();
@@ -51,9 +55,10 @@ void PID::driveStraight(int power) {
     master = frontRightDrive->get_position();
     partner = frontLeftDrive->get_position();
   }
-
+  //calculate the difference between the wheels from the two sides of the chassis
   int error = master - partner;
 
+  //decrease the side that is moving faster than the other
   if(abs(frontLeftDrive->get_position()) >= abs(frontRightDrive->get_position())) {
     powerLeft = power - (error * kp);
     powerRight = power;
@@ -63,6 +68,7 @@ void PID::driveStraight(int power) {
     powerRight = power - (error * kp);
   }
 
+  //check to make sure power is within constraints
   powerLeft = checkPower(powerLeft);
   powerRight = checkPower(powerRight);
 
@@ -72,6 +78,7 @@ void PID::driveStraight(int power) {
   backRightDrive->move(powerRight);
 }
 
+//makes sure robot moves to desired location
 void PID::distancePID(int targetDistance) {
   int kp = 0;
   int kd = 0;
@@ -80,6 +87,7 @@ void PID::distancePID(int targetDistance) {
   int derivative = 0;
   int lastError = 0;
   int power = 20;
+  //convert targetDistance from inches to degrees
   targetDistance = targetDistance / wheelCircumference * 360;
 
   setBrakeMode();
@@ -87,10 +95,12 @@ void PID::distancePID(int targetDistance) {
 
   while(error != 0) {
     currentDistance = (frontRightDrive->get_position() + frontLeftDrive->get_position()) / 2;
+    //calculate proportional error term and the derivative
     error = targetDistance - currentDistance;
     derivative = error - lastError;
     lastError = error;
 
+    //determine power and check to make sure the power is within constraints
     power = (error * kp) + (derivative * kd);
     power = checkPower(power);
 
